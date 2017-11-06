6d59daf821cafd4e87aeb14ed95f5d49628f5fc4
BuildTools <unconfigured@null.spigotmc.org>
2017-11-05 21:52:56 -0700

    Gives player control of camera

---
+++ CPI-211 Final Project/CPI-211 Final Project/Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+
+    //this script has a lot of inactive components that may need to be activated later
+    //right now it just gives the player the ability to turn the camera on its y axis
+    // Use this for initialization
+    public GameObject target;
+    public float cameraRotateSpeed;
+    //Vector3 offset;
+    
+	void Start () {
+        //offset = target.transform.position - transform.position;
+	}
+
+    // Update is called once per frame
+    void LateUpdate() {
+        float cameraHorizTurn = Input.GetAxis("Mouse X") * cameraRotateSpeed;
+        float cameraVertTurn = Input.GetAxis("Mouse Y") * cameraRotateSpeed;
+        //target.transform.Rotate(0, cameraTurn, 0); //activate for mouse turn to also control character
+
+        //float desiredAngle = target.transform.eulerAngles.y;
+        //Quaternion rotation = Quaternion.Euler(15, desiredAngle, 0);
+        
+        transform.Rotate(-cameraVertTurn, cameraHorizTurn, 0);
+        
+	}
+}
