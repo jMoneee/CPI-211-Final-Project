83ec380ca5eddca9a8ab3f23e283beec6a500ee5
Ishkablob <braydonshadow@gmail.com>
2017-11-21 18:54:58 -0700

    Menus
    added rough makeup of menus

---
+++ CPI-211 Final Project/Assets/Scripts/Controller_loadoutSelect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Controller_loadoutSelect : MonoBehaviour
+{
+    //public int players;
+
+    public Toggle p1Toggle, p2Toggle, p3Toggle, p4Toggle;
+    public GameObject ButtonPanel;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void SwitchScenesWithButton(string nextScene)
+    { SceneManager.LoadScene(nextScene); }
+
+
+
+}
---
+++ CPI-211 Final Project/Assets/Scripts/Controller_playersMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller_playersMenu : MonoBehaviour
+{
+    public void SwitchScenesWithButton(string nextScene)
+    { SceneManager.LoadScene(nextScene); }
+
+}
