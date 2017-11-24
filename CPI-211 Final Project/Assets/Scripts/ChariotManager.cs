80c4ce5399e30a746420357ffab23a78f4bd24ee
jMoneee <jdlarse1@asu.edu>
2017-11-24 16:58:13 -0700

    First Part of the loadout system
    -Put the chariot/horse options under their respective managers in the player objects

---
+++ CPI-211 Final Project/Assets/Scripts/ChariotManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChariotManager : MonoBehaviour {
+
+    public int selectedChariot = 0;
+    // Use this for initialization
+    void Start() {
+        SelectChariot();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void SelectChariot()
+    {
+        int i = 0;
+        foreach(Transform chariot in transform)
+        {
+            if (i == selectedChariot)
+            {
+                chariot.gameObject.SetActive(true);
+            }
+            else
+            {
+                chariot.gameObject.SetActive(false);
+            }
+            i++;
+        }
+    }
+}
