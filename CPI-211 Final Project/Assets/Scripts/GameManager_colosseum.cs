94df5fdf4828420104755b2a839a32d5e4d36bb4
Ishkablob <braydonshadow@gmail.com>
2017-11-29 23:58:28 -0700

    leaderboards
    leaderboards and minor ui changes

--- CPI-211 Final Project/Assets/Scripts/GameManager_colosseum.cs
+++ CPI-211 Final Project/Assets/Scripts/GameManager_colosseum.cs
@@ -6,30 +6,40 @@ using UnityEngine.SceneManagement;
 public class GameManager_colosseum : MonoBehaviour
 {
     private static int currentPlayers;
-    public static GameObject[] startingListOfPlayers;
+    public GameObject[] startingListOfPlayers;
+    public static int[] playerScoreList;
     public GameObject[] listOfPlayers;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         startingListOfPlayers = GameObject.FindGameObjectsWithTag("Player");
         listOfPlayers = startingListOfPlayers;
         setCurrentPlayers(listOfPlayers.Length);
+        playerScoreList = new int[startingListOfPlayers.Length];
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         listOfPlayers = GameObject.FindGameObjectsWithTag("Player");
         currentPlayers = listOfPlayers.Length;
 
-		if (currentPlayers == 1)
+        if (currentPlayers == 1)
         {
+            SetPlayerList();
             SceneManager.LoadScene("End Screen");
         }
-	}
+    }
+
+    void SetPlayerList()
+    {
+        for (int i = 0; i < Controller_playersMenu.getPlayerCount(); i++)
+        { playerScoreList[i] = startingListOfPlayers[i].GetComponent<Player_Controller>().getPlacement(); }
+    }
 
     public static int getCurrentPlayers() { return currentPlayers; }
     public static void setCurrentPlayers(int players) { currentPlayers = players; }
-    public static GameObject getPlayerFromList(int i) { return startingListOfPlayers[i]; }
+    public static int[] getPlayerScoreList() { return playerScoreList; }
+
 }
