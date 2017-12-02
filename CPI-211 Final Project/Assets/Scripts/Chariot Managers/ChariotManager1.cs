fc381fedbb65593b3f8878ebd45dcb78be6278d5
jMoneee <jdlarse1@asu.edu>
2017-12-01 19:27:13 -0700

    Added in all Loadout stuff completed so far

---
+++ CPI-211 Final Project/Assets/Scripts/Chariot Managers/ChariotManager1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChariotManager1 : MonoBehaviour {
+    //use this for player 2
+    public int selectedChariot = LoadoutManager.getP2chariot();
+    // Use this for initialization
+    void Start() {
+        SelectChariot();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void SelectChariot()
+    {
+        int i = 0;
+        foreach(Transform chariot in transform)
+        {
+            if (i == selectedChariot)
+            {
+                chariot.gameObject.SetActive(true);
+            }
+            else
+            {
+                chariot.gameObject.SetActive(false);
+            }
+            i++;
+        }
+    }
+}
---
+++ CPI-211 Final Project/Assets/Scripts/LoadoutManagers/LoadoutManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadoutManager : MonoBehaviour
+{
+    public static int P1chariot = 0;
+    public static int P1animal = 0;
+    public static int P2chariot = 0;
+    public static int P2animal = 0;
+    public static int P3chariot = 0;
+    public static int P3animal = 0;
+    public static int P4chariot = 0;
+    public static int P4animal = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+    
+    public static int getP1chariot()
+    {
+        return P1chariot;
+    }
+    public static int getP1animal()
+    {
+        return P1animal;
+    }
+    public static int getP2chariot()
+    {
+        return P2chariot;
+    }
+    public static int getP2animal()
+    {
+        return P2animal;
+    }
+    public static int getP3chariot()
+    {
+        return P3chariot;
+    }
+    public static int getP3animal()
+    {
+        return P3animal;
+    }
+    public static int getP4chariot()
+    {
+        return P4chariot;
+    }
+    public static int getP4animal()
+    {
+        return P4animal;
+    }
+    public static void setP1chariot(int i)
+    {
+        P1chariot =  i;
+    }
+    public static void setP1animal(int i)
+    {
+        P1animal = i;
+    }
+    public static void setP2chariot(int i)
+    {
+        P2chariot = i;
+    }
+    public static void setP2animal(int i)
+    {
+        P2animal = i;
+    }
+    public static void setP3chariot(int i)
+    {
+        P3chariot = i;
+    }
+    public static void setP3animal(int i)
+    {
+        P3animal = i;
+    }
+    public static void setP4chariot(int i)
+    {
+        P4chariot = i;
+    }
+    public static void setP4animal(int i)
+    {
+        P4animal = i;
+    }
+}
