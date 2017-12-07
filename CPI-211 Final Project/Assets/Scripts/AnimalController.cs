67c7052273995b5716791b178e2dd21f0fee18da
Brandon Bayles <bron546@gmail.com>
2017-12-06 20:56:38 -0700

    actual last one

--- CPI-211 Final Project/Assets/Scripts/AnimalController.cs
+++ CPI-211 Final Project/Assets/Scripts/AnimalController.cs
@@ -179,7 +179,7 @@ public class AnimalController : MonoBehaviour
         if (other.CompareTag("Caltrops_Deployed"))
         {
             Debug.Log("Caltrops run over!");
-            this.transform.parent.GetComponentInParent<Player_Controller>().reduceHealth(15);
+            this.transform.parent.GetComponentInParent<Player_Controller>().reduceHealth(40);
             Destroy(other.gameObject);
         }
         if (other.CompareTag("KillFloor"))
